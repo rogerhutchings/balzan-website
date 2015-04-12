5720054f8163aefbbf87c35dd93c86b4fea2c41c
Roger Hutchings <roger@hutchin.gs>
2015-04-12 22:15:50 +0100

    Fix formatting, use js- prefix for classes

--- public/js/grayscale.js
+++ public/js/grayscale.js
@@ -6,16 +6,16 @@
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function () {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    if ($('.navbar').offset().top > 50) {
+        $('.navbar-fixed-top').addClass('top-nav-collapse');
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $('.navbar-fixed-top').removeClass('top-nav-collapse');
     }
 });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function () {
-    $('a.js-page-scroll').bind('click', function(event) {
+    $('a.js-page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
@@ -25,6 +25,6 @@ $(function () {
 });
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+$('.navbar-collapse ul li a').click(function () {
     $('.navbar-toggle:visible').click();
 });
