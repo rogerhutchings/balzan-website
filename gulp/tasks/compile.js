8d59ad2aecb30baf38e5faa39c66e3a97708130f
Roger Hutchings <roger@hutchin.gs>
2015-04-27 12:07:54 +0100

    Add JS task to compile

--- gulp/tasks/compile.js
+++ gulp/tasks/compile.js
@@ -4,6 +4,6 @@ var config = require('../config.js');
 var gulp = require('gulp');
 var harp = require('harp');
 
-gulp.task('compile', function () {
+gulp.task('compile', ['javascript'], function () {
     return harp.compile(__dirname, config.build.dest, function () {});
 });
