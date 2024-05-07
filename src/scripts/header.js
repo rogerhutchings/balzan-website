ad95fef17e275415fa90df880637de17e232421a
Roger Hutchings <roger@hutchin.gs>
2024-05-07 23:59:54 +0100

    Package updates, fix up header

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -2,13 +2,16 @@ const pageTop = document.querySelector('[data-page-top]')
 const header = document.querySelector('[data-header]')
 
 const stuckClass = 'bg-black'
+const defaultClass = 'bg-transparent'
 
 // Add background classes to the header once we start scrolling
 function handler(entries) {
   if (header) {
     if (!entries[0].isIntersecting) {
       header.classList.add(stuckClass)
+      header.classList.remove(defaultClass)
     } else {
+      header.classList.add(defaultClass)
       header.classList.remove(stuckClass)
     }
   }
